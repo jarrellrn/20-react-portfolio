eeed9b43e88370a3fd73f38d34fb47578bdfde20
jarrellrn <jarrellrn@gmail.com>
2020-09-09 10:35:45 -0500

    updated home component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,1 +1,24 @@
 import React from "react";
+// import "../style.css";
+// import Nav from "../Nav";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div className="homeBody">
+      <h2>About Me</h2>
+      <div id="space1"></div>
+      <div id="container1">
+        <p>
+          Hello, I'm a 22 year old web design student focusing on full stack
+          development, with an interest in back-end design. I have experience in
+          HTML, CSS, JavaScript, NodeJS, MySQL, MongoDB, Mongoose, Sequelize,
+          Handlebars, Java, Kotlin, C++.
+        </p>
+      </div>
+      <div id="container12"></div>
+    </div>
+  );
+}
+
+export default Home;
