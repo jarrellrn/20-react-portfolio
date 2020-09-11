5a9d71644bd10dbacacd44136f83e8f7e5afd582
jarrellrn <jarrellrn@gmail.com>
2020-09-11 11:29:17 -0500

    more styling tweaks

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,11 +11,12 @@ function Home() {
       <h2>About Me</h2>
       <div id="space1"></div>
       <div id="container1">
-        <p>
+        <p className="mainText">
           Hello, I'm a 22 year old web design student focusing on full stack
-          development, with an interest in back-end design. I have experience in
-          HTML, CSS, JavaScript, NodeJS, MySQL, MongoDB, Mongoose, Sequelize,
-          Handlebars, Java, Kotlin, C++.
+          development, with an interest in back-end design. Technologies I have
+          experience in include HTML, CSS, JavaScript, NodeJS, React, MySQL,
+          Sequelize, MongoDB, Mongoose, Handlebars, with some dabbling in Java,
+          Kotlin, and C++.
         </p>
       </div>
       <div id="container12"></div>
