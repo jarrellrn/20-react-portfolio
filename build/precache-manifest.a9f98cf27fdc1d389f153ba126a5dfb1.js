d8ff495fcdf7c33562578383250e676f532eef33
jarrellrn <jarrellrn@gmail.com>
2020-09-11 11:55:35 -0500

    added build and deleted procfile

---
+++ build/precache-manifest.a9f98cf27fdc1d389f153ba126a5dfb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a9756ad4d002b5843d5abea2152408",
+    "url": "/index.html"
+  },
+  {
+    "revision": "120e867da34116c62550",
+    "url": "/static/css/main.77b9c331.chunk.css"
+  },
+  {
+    "revision": "7f9d8943555ed742299c",
+    "url": "/static/js/2.d2c6c053.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d2c6c053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "120e867da34116c62550",
+    "url": "/static/js/main.d65acd2f.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  }
+]);
